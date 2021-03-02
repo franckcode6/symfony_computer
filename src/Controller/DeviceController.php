cb07ff81879bf19b8c8e14d0741445b899ec10e2
franckcode6 <franck.quasevi@yahoo.com>
2021-03-02 15:29:26 +0100

    Device form

--- src/Controller/DeviceController.php
+++ src/Controller/DeviceController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Device;
+use App\Form\DeviceType;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +22,28 @@ class DeviceController extends AbstractController
     {
         return $this->render('device/index.html.twig', [
             'controller_name' => 'DeviceController',
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(): Response
+    {
+        $device = new Device();
+
+        $form = $this->createForm(DeviceType::class, $device);
+        /*$form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($device);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('device_index');
+        }*/
+
+        return $this->render('device/new.html.twig', [
+            'formDevice' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/DeviceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Device;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeviceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('price', MoneyType::class)
+            ->add('brand', TextType::class)
+            ->add('type', ChoiceType::class, [
+                'choices' => Device::AVAILABLE_TYPES,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Device::class,
+        ]);
+    }
+}
