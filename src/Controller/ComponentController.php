bf417530079136f6ea92165b1a69ad9a4240e711
Hermier Evan <evan.hermier@gmail.com>
2021-03-04 09:49:28 +0100

    add fonctionality new

--- src/Controller/ComponentController.php
+++ src/Controller/ComponentController.php
@@ -4,17 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Component;
 use App\Form\ComponentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/component", name="component")
+ * @Route("/component", name="component_")
  */
 class ComponentController extends AbstractController
 {
     /**
-     * @Route("/", name="component_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -24,13 +26,27 @@ class ComponentController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="component_new")
+     * @Route("/new", name="new")
      */
-    public function new(): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
         $component = new Component();
 
-        $form = $this->createForm(ComponentType::class, $component);
+        $form = $this->createForm(ComponentType::class, $component, [
+            'method' => 'POST',
+            'action' => $this->generateUrl('component_new'),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($component);
+            $em->flush();
+
+            return $this->redirectToRoute('component_index');
+        }
+
 
         return $this->render('component/new.html.twig', [
             'formComponent' => $form->createView(),
