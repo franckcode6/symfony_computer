d7cac6d595070932f24aec33642510b9269acfe8
Elwilya <69197349+Elwilya@users.noreply.github.com>
2021-03-04 13:42:38 +0100

    Add computeur Form

--- src/Controller/ComputerController.php
+++ src/Controller/ComputerController.php
@@ -3,35 +3,73 @@
 namespace App\Controller;
 
 use App\Entity\Computer;
+use App\Entity\Device;
 use App\Form\ComputerType;
+use App\Repository\ComputerRepository;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/computer", name="computer")
+ * @Route("/computer", name="computer_")
  */
 class ComputerController extends AbstractController
 {
     /**
-     * @Route("/", name="computer_index")
+     * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(ComputerRepository $computerRepository): Response
     {
+        $computer = $computerRepository->findAll();
         return $this->render('computer/index.html.twig', [
-            'controller_name' => 'ComputerController',
+            'computers' => $computer]);
+    }
+
+    /**
+     * @Route("/new", name="new")
+     * @Route("/{id}/edit", name="edit")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param Computer|null $computer
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager, Computer $computer = null): Response
+    {
+        if (empty($computer)) {
+            $computer = new Computer();
+        }
+
+        $form = $this->createForm(ComputerType::class, $computer, [
+            'method' => 'POST',
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $computer->setUpdatedAt(new DateTime());
+            $entityManager->persist($computer);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('computer_index');
+        }
+
+        return $this->render('computer/new.html.twig', [
+            'formComputer' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/new", name="computer_new")
-     */
-    public function new(): Response
+         * @Route("/{id}/remove", name="remove")
+         */
+    public function remove(Computer $computer, EntityManagerInterface $entityManager)
     {
-        $computer = new Computer();
-        $form = $this->createForm(ComputerType::class, $computer);
-        return $this->render('computer/new.html.twig', [
-            'formComputer' => $form->createView(),
-        ]);
+        $entityManager->remove($computer);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('computer_index');
     }
 }
